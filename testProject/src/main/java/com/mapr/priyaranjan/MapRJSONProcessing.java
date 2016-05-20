e5e83cf9ccc095bed38fdff6ff6b1320d8e8d4a0
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-05-20 16:38:53 -0700

    Adding file to start off processing JSON.

---
+++ testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
@@ -0,0 +1,34 @@
+package com.mapr.priyaranjan;
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.thrift.generated.Hbase.createTable_args;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+public class MapRJSONProcessing {
+	
+  @SuppressWarnings("deprecation")
+public static void main(String[] args) throws IOException {
+    
+    try {
+    	
+     }
+    finally {
+       //connection.close();
+     }
+    
+  }
+}
