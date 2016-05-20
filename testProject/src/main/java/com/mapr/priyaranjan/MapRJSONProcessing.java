938289923059fe753f626fcb02057962f11df8bf
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-05-20 16:53:29 -0700

    Adding changes to read the json file

--- testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
+++ testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
@@ -1,6 +1,8 @@
 package com.mapr.priyaranjan;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -23,12 +25,11 @@ public class MapRJSONProcessing {
   @SuppressWarnings("deprecation")
 public static void main(String[] args) throws IOException {
     
-    try {
-    	
-     }
-    finally {
-       //connection.close();
-     }
+	  Scanner sc = new Scanner(new File("/tmp/zips.json"));
+	  
+	  while(sc.hasNext()){
+	        System.out.println(sc.nextLine());
+	  }
     
   }
 }
