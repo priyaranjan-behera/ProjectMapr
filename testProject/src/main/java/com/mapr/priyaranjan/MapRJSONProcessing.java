80036da5c9c32b47657b192ce88251e8f238eba0
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-05-20 17:32:06 -0700

    Test

--- testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
+++ testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
@@ -17,8 +17,10 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.ojai.Document;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import com.mapr.db.MapRDB;
 
 public class MapRJSONProcessing {
 	
@@ -26,10 +28,19 @@ public class MapRJSONProcessing {
 public static void main(String[] args) throws IOException {
     
 	  Scanner sc = new Scanner(new File("/tmp/zips.json"));
+	  String curJSON;
 	  int i = 0;
 	  while(sc.hasNext()){
-		  	System.out.println("This is line: " + Integer.toString(i));
-	        System.out.println(sc.nextLine());
+		  	System.out.println("This is line: " + Integer.toString(i++));
+	        curJSON = new String(sc.nextLine());
+	        
+	        Document document = MapRDB.newDocument(curJSON);
+	        
+	        System.out.println("Verifing if the document captured things:");
+	        System.out.println("City: " + document.getString("city"));
+	        System.out.println("Pop: " + document.getInt("pop"));
+	        System.out.println("Id: " + document.getInt("_id"));
+	        
 	  }
 	  
 	  sc.close();
