c8a26d6d14cc1d75f99c4131fac904eb4eac3df4
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-05-20 17:34:32 -0700

    Test

--- testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
+++ testProject/src/main/java/com/mapr/priyaranjan/MapRJSONProcessing.java
@@ -39,7 +39,7 @@ public static void main(String[] args) throws IOException {
 	        System.out.println("Verifing if the document captured things:");
 	        System.out.println("City: " + document.getString("city"));
 	        System.out.println("Pop: " + document.getDouble("pop"));
-	        System.out.println("Id: " + document.getDouble("_id"));
+	        System.out.println("Id: " + document.getString("_id"));
 	        
 	  }
 	  
