55644bc87b424db390292b7cbb7873ae25364332
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-05-22 23:20:03 -0700

    Changes to read from json and add it to the hbase table.

--- testProject/src/main/java/com/mapr/priyaranjan/MapRDBTabularClient.java
+++ testProject/src/main/java/com/mapr/priyaranjan/MapRDBTabularClient.java
@@ -1,6 +1,7 @@
 package com.mapr.priyaranjan;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -50,6 +51,45 @@ public class MapRDBTabularClient {
 	}
 	
 	
+	public static void createTableforZipJSON(String tableName)
+	{
+		try{
+
+			// Reads the configurations from the conf folder as mentioned in the classpath. 
+			Configuration config = HBaseConfiguration.create();
+			
+			// Lets create a HBaseAdmin here from the config
+			HBaseAdmin admin = new HBaseAdmin(config);
+			
+			//creating table descriptor
+			HTableDescriptor table = new HTableDescriptor(Bytes.toBytes("/tmp/zip_table"));
+	    	
+	    	//creating column family descriptor
+	    	HColumnDescriptor family = new HColumnDescriptor(Bytes.toBytes("Identification"));
+	    	//adding column family to HTable
+	    	table.addFamily(family);
+	    	
+	    	//creating column family descriptor
+	    	family = new HColumnDescriptor(Bytes.toBytes("Stats"));
+	    	//adding column family to HTable
+	    	table.addFamily(family);
+	    	
+	    	//creating column family descriptor
+	    	family = new HColumnDescriptor(Bytes.toBytes("Location"));
+	    	//adding column family to HTable
+	    	table.addFamily(family);
+	    	
+	    	admin.createTable(table);
+	    	
+		}catch(Exception e)
+		{
+			System.out.println("Error while creating table: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	
 	public static void addDataToTable(String tableName)
 	{
@@ -66,6 +106,54 @@ public class MapRDBTabularClient {
 				Put p = new Put(Bytes.toBytes("Row2"));
 				p.add(Bytes.toBytes("column family"), Bytes.toBytes("column2"),Bytes.toBytes("12"));
 				table.put(p);
+				
+			} catch (Exception e)
+			{
+				System.out.println("Error while writing to table: " + e.getMessage());
+				e.printStackTrace();
+			}finally {
+				connection.close();
+		    }
+			
+		} catch (IOException e) {
+			System.out.println("Couldn't connect to the cluster: " + e.getMessage());
+			e.printStackTrace();
+		}
+	    
+	}
+	
+	public static void addDataToTableFromJSON(String fileName, String tableName)
+	{
+		// Reads the configurations from the conf folder as mentioned in the classpath. 
+	    Configuration config = HBaseConfiguration.create();
+	    
+	    //From the configuration we create a connection to the cluster. 
+	    try {
+			Connection connection = ConnectionFactory.createConnection(config);
+			Table table = connection.getTable(TableName.valueOf("/tmp/java_table"));
+			List<JSONStructure> data = getDataFromFile("/tmp/zips.json");
+			
+			try {
+				for(JSONStructure row:data)
+				{
+					Put p = new Put(Bytes.toBytes(row.getId()));
+					p.add(Bytes.toBytes("Identification"), Bytes.toBytes("id"),Bytes.toBytes(row.getId()));
+					table.put(p);
+					
+					p.add(Bytes.toBytes("Identification"), Bytes.toBytes("city"),Bytes.toBytes(row.getCity()));
+					table.put(p);
+					
+					
+					p.add(Bytes.toBytes("Stats"), Bytes.toBytes("pop"),Bytes.toBytes(row.getPop()));
+					table.put(p);
+					
+					
+					p.add(Bytes.toBytes("Location"), Bytes.toBytes("loc1"),Bytes.toBytes(row.getLocation().get(0)));
+					table.put(p);
+					
+					p.add(Bytes.toBytes("Location"), Bytes.toBytes("loc2"),Bytes.toBytes(row.getLocation().get(1)));
+					table.put(p);
+				}
 				
 			} catch (Exception e)
 			{
